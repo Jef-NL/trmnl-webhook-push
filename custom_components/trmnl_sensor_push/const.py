e4c0fa12c4c905ae62041674de585c311732e081
Jef <71375518+Jef-NL@users.noreply.github.com>
2026-06-05 21:05:12 +0200

    feat: add TRMNL_LABEL_NAME constant

--- custom_components/trmnl_sensor_push/const.py
+++ custom_components/trmnl_sensor_push/const.py
@@ -2,4 +2,5 @@
 DOMAIN = "trmnl_sensor_push"
 CONF_URL = "url"
 DEFAULT_URL = "https://usetrmnl.com/api/custom_plugins/XXXX-XXXX-XXXX-XXXX"  # Example URL
-MIN_TIME_BETWEEN_UPDATES = 450  # 300 is 5 minutes in seconds, 1800 is every 30 mins
+MIN_TIME_BETWEEN_UPDATES = 1800  # 300 is 5 minutes in seconds, 1800 is every 30 mins
+TRMNL_LABEL_NAME = "TRMNL"
